b26c89004b4efe31f9416f7d57c50c06e382acd6
Matt Pityo <mpityo@gmail.com>
2022-03-10 16:31:53 -0500

    db queries for getting, deleting and creating

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,47 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'mintcake',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// CREATE a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Fairbank', 1];
+
+// db.query(sql, params, (err, results) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(results);
+// });
 
 // default response for any other request (not found)
 // leave as last route, or will overwrite others
